4db2b1451c7366454a33b0545e24e7f00e3be259
MullakhmetovDanil <danilmullakhmetov@gmail.com>
2024-07-12 21:50:40 +0300

    Step 3 (Flat File Comparison)

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,8 +1,14 @@
 import json
 
-file1_path = '/python-project-50/gendiff/dir_with_json/file1.json'
-file2_path = '/python-project-50/gendiff/dir_with_json/file1.json'
+file1_path = '/python-project-50/gendiff/dir_with_jsons/file1.json'
+file2_path = '/python-project-50/gendiff/dir_with_jsons/file2.json'
 
+def convert_value_to_string(value):
+    if value is None:
+        return "none"
+    elif isinstance(value, bool):
+        return str(value).lower()
+    return value
 
 def generate_diff(file1_path, file2_path):
     with open(file1_path, 'r') as file1, open(file2_path, 'r') as file2:
@@ -15,15 +21,15 @@ def generate_diff(file1_path, file2_path):
 
     for key in sorted(keys):
         if key not in data2:
-            diff_lines.append(f"  - {key}: {data1[key]}")
+            diff_lines.append(f"  - {key}: {convert_value_to_string(data1[key])}")
         elif key not in data1:
-                diff_lines.append(f"  + {key}: {data2[key]}")
+            diff_lines.append(f"  + {key}: {convert_value_to_string(data2[key])}")
         else:
-                if data1[key] != data2[key]:
-                    diff_lines.append(f"  - {key}: {data1[key]}")
-                    diff_lines.append(f"  + {key}: {data2[key]}")
-                else:
-                    diff_lines.append(f"    {key}: {data1[key]}")
+            if data1[key] != data2[key]:
+                diff_lines.append(f"  - {key}: {convert_value_to_string(data1[key])}")
+                diff_lines.append(f"  + {key}: {convert_value_to_string(data2[key])}")
+            else:
+                diff_lines.append(f"    {key}: {convert_value_to_string(data1[key])}")
     
     diff_result = "{\n" + "\n".join(diff_lines) + "\n}"
     
