95032adf0fd1e79692bc43916bed58388ae60545
MullakhmetovDanil <danilmullakhmetov@gmail.com>
2024-07-23 20:44:27 +0300

    test fail build package

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,39 +1,45 @@
 import json
 import yaml
 
-def convert_value_to_string(value):
+
+def conv_string(value):
     if isinstance(value, bool):
         return str(value).lower()
     elif value is None:
         return "null"
     return str(value)
 
-def generate_diff(file1_path, file2_path):
-    with open(file1_path, 'r') as file1, open(file2_path, 'r') as file2:
-        if file1_path.endswith('.json') and file2_path.endswith('.json'):
-            data1 = json.load(file1)
-            data2 = json.load(file2)
-        elif file1_path.endswith('.yaml') and file2_path.endswith('.yaml'):
-            data1 = yaml.safe_load(file1)
-            data2 = yaml.safe_load(file2)
+
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        if file_path.endswith('.json'):
+            return json.load(file)
+        elif file_path.endswith('.yaml'):
+            return yaml.safe_load(file)
         else:
             raise ValueError("Unsupported file format")
-    
-        keys = set(data1.keys()).union(set(data2.keys()))
-    
-        diff_lines = []
-        for key in sorted(keys):
-            if key not in data2:
-                diff_lines.append(f"  - {key}: {convert_value_to_string(data1[key])}")
-            elif key not in data1:
-                diff_lines.append(f"  + {key}: {convert_value_to_string(data2[key])}")
+
+
+def generate_diff_lines(data1, data2):
+    keys = set(data1.keys()).union(set(data2.keys()))
+    diff_lines = []
+    for key in sorted(keys):
+        if key not in data2:
+            diff_lines.append(f"  - {key}: {conv_string(data1[key])}")
+        elif key not in data1:
+            diff_lines.append(f"  + {key}: {conv_string(data2[key])}")
+        else:
+            if data1[key] != data2[key]:
+                diff_lines.append(f"  - {key}: {conv_string(data1[key])}")
+                diff_lines.append(f"  + {key}: {conv_string(data2[key])}")
             else:
-                if data1[key] != data2[key]:
-                    diff_lines.append(f"  - {key}: {convert_value_to_string(data1[key])}")
-                    diff_lines.append(f"  + {key}: {convert_value_to_string(data2[key])}")
-                else:
-                    diff_lines.append(f"    {key}: {convert_value_to_string(data1[key])}")
-    
-        diff_result = "{\n" + "\n".join(diff_lines) + "\n}"
-    
+                diff_lines.append(f"    {key}: {conv_string(data1[key])}")
+    return diff_lines
+
+
+def generate_diff(file1_path, file2_path):
+    data1 = read_file(file1_path)
+    data2 = read_file(file2_path)
+    diff_lines = generate_diff_lines(data1, data2)
+    diff_result = "{\n" + "\n".join(diff_lines) + "\n}"
     return diff_result
