5bfe4fa9867d1fa86498d9dcec87e7aa6d32495d
MullakhmetovDanil <danilmullakhmetov@gmail.com>
2024-08-17 16:42:57 +0300

    Cleaning Gendiff from additional formats

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,15 +5,7 @@ from gendiff.helpers.cli import parse_arguments
 
 def main():
     args = parse_arguments()
-    if args.format == 'plain':
-        formatter = 'plain'
-    elif args.format == 'json':
-        formatter = 'json'
-    elif args.format == 'stylish':
-        formatter = 'stylish'
-    else:
-        raise ValueError(f"Unknown format: {args.format}")
-    print(generate_diff(args.first_file, args.second_file, formatter))
+    print(generate_diff(args.first_file, args.second_file, args.formatter))
 
 
 if __name__ == "__main__":
