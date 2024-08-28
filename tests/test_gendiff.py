f2094092c4045b5c67d83446b7030ca012d74d60
MullakhmetovDanil <danilmullakhmetov@gmail.com>
2024-08-28 16:10:58 +0300

    Combining testing functions; Added test for 'JSON' format

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,37 +1,71 @@
-from gendiff.engine import generate_diff
 import pytest  # type: ignore
+import json
+from gendiff.engine import generate_diff
 
-
-@pytest.mark.parametrize("file1,file2", [
-    ('tests/fixtures/file1.json', 'tests/fixtures/file2.json'),
-    ('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml')
-])
-def test_generate_diff(file1, file2):
-    expected_output = open('tests/fixtures/result_json_1_2.txt').read()
-    different = generate_diff(file1, file2)
+@pytest.mark.parametrize(
+    "file1, file2, expected_output_file, format_type",
+    [
+        (
+            'tests/fixtures/file1.json',
+            'tests/fixtures/file2.json',
+            'tests/fixtures/result_json_1_2.txt',
+            None
+        ),
+        (
+            'tests/fixtures/file1.yaml',
+            'tests/fixtures/file2.yaml',
+            'tests/fixtures/result_json_1_2.txt',
+            None
+        ),
+        (
+            'tests/fixtures/file3.json',
+            'tests/fixtures/file4.json',
+            'tests/fixtures/result_json_3_4.txt',
+            None
+        ),
+        (
+            'tests/fixtures/file3.yaml',
+            'tests/fixtures/file4.yaml',
+            'tests/fixtures/result_json_3_4.txt',
+            None
+        ),
+        (
+            'tests/fixtures/file3.json',
+            'tests/fixtures/file4.json',
+            'tests/fixtures/result_plain.txt',
+            'plain'
+        ),
+        (
+            'tests/fixtures/file3.yaml',
+            'tests/fixtures/file4.yaml',
+            'tests/fixtures/result_plain.txt',
+            'plain'
+        ),
+        (
+            'tests/fixtures/file3.json',
+            'tests/fixtures/file4.json',
+            'tests/fixtures/result_json_json.txt',
+            'json'
+        ),
+        (
+            'tests/fixtures/file3.yaml',
+            'tests/fixtures/file4.yaml',
+            'tests/fixtures/result_json_json.txt',
+            'json'
+        )
+    ]
+)
+def test_generate_diff(file1, file2, expected_output_file, format_type):
+    with open(expected_output_file) as f:
+        expected_output = f.read()
+    if format_type:
+        different = generate_diff(file1, file2, format_type)
+    else:
+        different = generate_diff(file1, file2)
     assert isinstance(different, str)
-    assert different == expected_output
-
-
-@pytest.mark.parametrize("file1,file2", [
-    ('tests/fixtures/file3.json', 'tests/fixtures/file4.json'),
-    ('tests/fixtures/file3.yaml', 'tests/fixtures/file4.yaml')
-])
-def test_generate_diff_rec(file1, file2):
-    expected_output = open('tests/fixtures/result_json_3_4.txt').read()
-    different = generate_diff(file1, file2)
-    assert isinstance(different, str)
-    assert different == expected_output
-
-
-@pytest.mark.parametrize("file1,file2", [
-    ('tests/fixtures/file3.json', 'tests/fixtures/file4.json'),
-    ('tests/fixtures/file3.yaml', 'tests/fixtures/file4.yaml')
-])
-def test_generate_diff_plain(file1, file2):
-    expected_output = open('tests/fixtures/result_plain.txt').read()
-    different = generate_diff(file1, file2, 'plain')
-    assert isinstance(different, str)
-    assert generate_diff(
-        'tests/fixtures/file3.yaml', 'tests/fixtures/file4.yaml', 'plain'
-    ) == expected_output
+    if format_type == 'json':
+        different_json = json.loads(different)
+        expected_json = json.loads(expected_output)
+        assert different_json == expected_json
+    else:
+        assert different == expected_output
