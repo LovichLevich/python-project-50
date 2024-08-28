ac15a7fe52c4faa82f47fcc959a21412e3e9085b
MullakhmetovDanil <danilmullakhmetov@gmail.com>
2024-08-28 12:38:27 +0300

    refactor: remove chois_filtter.py and move its functions to engine.py

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,6 +1,21 @@
 from gendiff.data import read_file_data
-from gendiff.chois_filtter import determine_format
 from gendiff.parser import parse
+from gendiff.formatters.json import json
+from gendiff.formatters.plain import plain
+from gendiff.formatters.stylish import stylish
+
+ERROR_MESSAGE = 'Error! Wrong output format'
+
+
+def determine_format(string_format):
+    if string_format == 'stylish' or not string_format:
+        return stylish
+    elif string_format == 'plain':
+        return plain
+    elif string_format == 'json':
+        return json
+    else:
+        raise ValueError(ERROR_MESSAGE)
 
 
 def gen_different(item1, item2):
